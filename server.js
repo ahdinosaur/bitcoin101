47687ddb99a33f9b3c6fb24d66d1c690fc61d88c
Michael Williams <dinosaur@riseup.net>
2014-10-19 15:51:43 -0700

    use ecstatic instead of serve-static

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var app = require('connect')();
 
-app.use(require('serve-static')('./build'));
+app.use(require('ecstatic')({
+  root: __dirname + "/build",
+  baseDir: "/",
+  cache: 0,
+  showDir: false,
+}));
 
 app.listen(5000);
